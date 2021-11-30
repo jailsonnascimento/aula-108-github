678e18b9f5f6c923b1caf9c693c19151402b14ba
jailsonnascimento <jsn.santosnascimento@gmail.com>
2021-11-29 21:30:51 -0300

    Acrecentado mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Bom dia");
+		
+		sc.close();
+		
 	}
 
 }
